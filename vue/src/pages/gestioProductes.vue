09308858613d3d879d61180911f64451bc666e94
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2024-10-21 13:07:28 +0200

    paginas creadas

---
+++ vue/src/pages/gestioProductes.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Gestio de Productes</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'gestioProductes'
+}
+</script>
+
+<style>
+
+</style>
--- vue/src/pages/index.vue
+++ vue/src/pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <HelloWorld />
+  <nav>
+    <router-link to="/gestioProductes">Gestio de Productes</router-link>
+    <router-link to="/gestioComandes">Gestio de Comandes</router-link>
+
+    <router-view />
+
+    <div>
+      <h1>Comandes en curs</h1>
+    </div>
+  </nav>
 </template>
 
 <script setup>
