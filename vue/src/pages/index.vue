39abb033d996c0fc3e36985ed534851e00d655bb
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2024-10-22 10:04:09 +0200

    links añadidos

--- vue/src/pages/index.vue
+++ vue/src/pages/index.vue
@@ -2,7 +2,14 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>Comandes en curs</v-toolbar-title>
-    </v-app-bar>
+      <v-spacer></v-spacer>
+      <router-link to="/gestioComandes" style="text-decoration: none; color: white;">
+        <v-btn text @click="irAGestioComandes">Gestió Comandes</v-btn>
+      </router-link>
+      <router-link to="/gestioProductes" style="text-decoration: none; color: white;">
+        <v-btn text @click="irAGestioProductes">Gestió Productes</v-btn>
+      </router-link>
+    </v-app-bar>    
 
     <v-main>
       <v-container class="mt-10">
