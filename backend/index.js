a5d43c929237ecf2752db90e80e47ed1e088c8fb
a23alechasan <a23alechasan@inspedralbes.cat>
2024-10-22 12:45:22 +0200

    ConnexioLocal

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,64 @@
+const { json } = require('express');
+
 var mysql = require('mysql');
 
+var usuaris = [{}];
+var productes = [{}];
+var comandes = [{}];
+var itemsComandes = [{}]
+
 var con = mysql.createConnection({
-  host: "daw.inspedralbes.cat",
-  user: "a23alechasan_PR1",
-  password: "Skogsvardet_2024",
+  host: "localhost",
+  user: "root",
+  password: "",
   database: 'a23alechasan_PR1'
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
+  getUsers();
+  getProductes();
+  getItemsComandes();
+  getComandes();
 });
+
+function getUsers() {
+  con.query('SELECT * FROM users', (err, results, fields) => {
+    if (err) {
+      console.error('Error:', err);
+    } else {
+      usuaris.push(results);
+    }
+  });
+}
+
+function getProductes() {
+  con.query('SELECT * FROM products', (err, results, fields) => {
+    if (err) {
+      console.error('Error:', err);
+    } else {
+      productes.push(results);
+    }
+  });
+}
+
+function getComandes() {
+  con.query('SELECT * FROM orders', (err, results, fields) => {
+    if (err) {
+      console.error('Error:', err);
+    } else {
+      comandes.push(results);
+    }
+  });
+}
+
+function getItemsComandes() {
+  con.query('SELECT * FROM orderitems', (err, results, fields) => {
+    if (err) {
+      console.error('Error:', err);
+    } else {
+      itemsComandes.push(results);
+    }
+  });
+}
