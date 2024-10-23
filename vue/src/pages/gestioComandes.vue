a7c016abaac6ada0f59d8ad168b2585d937bccad
Fabianroldan <a21fabrolfer@inspedralbes.cat>
2024-10-23 09:48:22 +0200

    header gestioComandes

--- vue/src/pages/gestioComandes.vue
+++ vue/src/pages/gestioComandes.vue
@@ -1,7 +1,16 @@
 <template>
-    <div>
-        <h1>Gestio de Comandes</h1>
-    </div>
+    <v-app>
+        <v-app-bar app color="primary" dark>
+            <v-toolbar-title>Gestió de comandes</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <router-link to="/gestioComandes" style="text-decoration: none; color: white;">
+                <v-btn text @click="irAGestioComandes">Gestió Comandes</v-btn>
+            </router-link>
+            <router-link to="/gestioProductes" style="text-decoration: none; color: white;">
+                <v-btn text @click="irAGestioProductes">Gestió Productes</v-btn>
+            </router-link>
+        </v-app-bar>
+    </v-app>
 </template>
 
 <script>
@@ -10,6 +19,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
