76de1716cc4491392516158c1d4497f1f9ed62aa
a23alechasan <a23alechasan@inspedralbes.cat>
2024-10-24 09:49:02 +0200

    Endpoints per actualitzar l'estat de les comandes i reorganitzacio del document en general

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+/*<-------------------------------------- Variables ---------------------------------------->*/
+
 const { json } = require('express');
 const express = require('express');
 const cors = require('cors');
@@ -17,6 +19,8 @@ var productes = [];
 var comandes = [];
 
 
+/*<-------------------------------------- Connexions ---------------------------------------->*/
+
 /* var pool = mysql.createPool({
   host: "localhost",
   user: "root",
@@ -24,7 +28,7 @@ var comandes = [];
   database: 'a23alechasan_PR1',
   port: 3306,
   connectionLimit: 10 
-});  */
+}); */
 
 var pool = mysql.createPool({
   host: 'localhost',
@@ -35,23 +39,22 @@ var pool = mysql.createPool({
   connectionLimit: 10 
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
-pool.getConnection((err, connection) => {
-  if (err) {
-    console.error('Error getting connection from pool:', err);
-    return;
+/*<-------------------------------------- Usuaris ---------------------------------------->*/
+
+app.get("/getUsuaris", (req, res) => {
+  if (req.query.user_id) {
+    const idUsuari = Number(req.query.id);
+    for (const usuari of usuaris) {
+      if (usuari.user_id == idUsuari) {
+        res.json(usuari);
+      } else {
+        res.send(`No hi ha cap usuari amb id: ${idProducte}`);
+      }
+    }
+  } else {
+    res.json(usuaris);
   }
-  
-  console.log("Connected to the pool!");
-
-  getUsers(connection);
-  getProductes(connection);
-  getComandes(connection);
-
-  connection.release();
 });
 
 app.post("/createUsuari", (req, res) => {
@@ -87,7 +90,7 @@ app.post("/createUsuari", (req, res) => {
 });
 
 app.delete("/deleteUsuari", (req, res) => {
-  const idUserEliminar = req.query.id
+  const idUserEliminar = req.query.user_id
 
   pool.getConnection((err, connection) => {
     if (err) {
@@ -112,23 +115,43 @@ app.delete("/deleteUsuari", (req, res) => {
   });
 });
 
-app.get("/getUsuaris", (req, res) => {
-  if (req.query.id) {
-    const idUsuari = Number(req.query.id);
-    for (const usuari of usuaris) {
-      if (usuari.user_id == idUsuari) {
-        res.json(usuari);
-      } else {
-        res.send(`No hi ha cap usuari amb id: ${idProducte}`);
-      }
+app.put("/updateUsuari", (req, res) => {
+  const user = {
+    user_id: req.query.user_id,
+    username: req.query.username,
+    password: req.query.password,
+    first_name: req.query.first_name,
+    last_name: req.query.last_name,
+    email: req.query.email
+  };
+  
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error getting connection from pool:', err);
+      res.status(500).send("Error al obtenir connexió");
+      return;
     }
-  } else {
-    res.json(usuaris);
-  }
+
+    const query = `UPDATE Users SET username = ?, password = ?, first_name = ?, last_name = ?, email = ?  WHERE user_id = ?`;
+  
+    connection.query(query, [user.username, user.password, user.first_name, user.last_name, user.email, user.user_id], (err, results) => {
+      if (err) {
+        console.error('Error:', err);
+        res.status(500).send("Error en actualitzar l'usuari");
+      } else {
+        getUsers(connection);
+        res.send("Usuari actualitzat!");
+        console.log(`Usuari: ${user.username} actualitzat correctament!`)
+      }
+      connection.release();
+    }); 
+  });
 });
 
+/*<-------------------------------------- Productes ---------------------------------------->*/
+
 app.get("/getProductes", (req, res) => {
-  if (req.query.id) {
+  if (req.query.product_id) {
     const idProducte = Number(req.query.id);
     for (const producte of productes) {
       if (producte.product_id == idProducte) {
@@ -142,8 +165,10 @@ app.get("/getProductes", (req, res) => {
   }
 });
 
+/*<-------------------------------------- Comandes ---------------------------------------->*/
+
 app.get("/getComandes", (req, res) => {
-  if (req.query.id) {
+  if (req.query.order_id) {
     const idComanda = Number(req.query.id);
     for (const comanda of comandes) {
       if (comanda.order_id == idComanda) {
@@ -155,6 +180,106 @@ app.get("/getComandes", (req, res) => {
   } else {
     res.json(comandes);
   }
+});
+
+app.put("/verified", (req, res) => {
+  const order_id = req.query.order_id
+  
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error getting connection from pool:', err);
+      res.status(500).send("Error al obtenir connexió");
+      return;
+    }
+
+    const query = `UPDATE Orders SET status = 'verified' WHERE order_id = ?`;
+  
+    connection.query(query, [order_id], (err, results) => {
+      if (err) {
+        console.error('Error:', err);
+        res.status(500).send("Error en actualitzar l'ordre");
+      } else {
+        getComandes(connection);
+        res.send("Ordre actualitzada a 'verified'!");
+        console.log(`L'ordre: ${order_id} ha estat verificada`)
+      }
+      connection.release();
+    }); 
+  });
+});
+
+app.put("/confirmed", (req, res) => {
+  const order_id = req.query.order_id
+  
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error getting connection from pool:', err);
+      res.status(500).send("Error al obtenir connexió");
+      return;
+    }
+
+    const query = `UPDATE Orders SET status = 'confirmed' WHERE order_id = ?`;
+  
+    connection.query(query, [order_id], (err, results) => {
+      if (err) {
+        console.error('Error:', err);
+        res.status(500).send("Error en actualitzar l'ordre");
+      } else {
+        getComandes(connection);
+        res.send("Ordre actualitzada a 'confirmed'!");
+        console.log(`L'ordre: ${order_id} ha estat confirmada`)
+      }
+      connection.release();
+    }); 
+  });
+});
+
+app.put("/canceled", (req, res) => {
+  const order_id = req.query.order_id
+  
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error getting connection from pool:', err);
+      res.status(500).send("Error al obtenir connexió");
+      return;
+    }
+
+    const query = `UPDATE Orders SET status = 'canceled' WHERE order_id = ?`;
+  
+    connection.query(query, [order_id], (err, results) => {
+      if (err) {
+        console.error('Error:', err);
+        res.status(500).send("Error en actualitzar l'ordre");
+      } else {
+        esborrarComanda(connection, order_id)
+        getComandes(connection);
+        res.send("Ordre actualitzada a 'canceled'!");
+        console.log(`L'ordre: ${order_id} ha estat cancelada`)
+      }
+      connection.release();
+    }); 
+  });
+});
+
+/*<-------------------------------------- Inici App ---------------------------------------->*/
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error getting connection from pool:', err);
+    return;
+  }
+  
+  console.log("Connected to the pool!");
+
+  getUsers(connection);
+  getProductes(connection);
+  getComandes(connection);
+
+  connection.release();
 });
 
 function getUsers(connection) {
@@ -184,5 +309,18 @@ function getComandes(connection) {
     } else {
       comandes = results;
     }
+  });
+}
+
+function esborrarComanda(connection ,order_id) {
+  const query = `DELETE FROM Orders WHERE order_id=?;`;
+  connection.query(query, [order_id], (err, results) => {
+    if (err) {
+      console.error('Error:', err);
+    } else {
+      getUsers(connection);
+      console.log(`Ordre amb id: ${order_id} eliminada correctament!`)
+    }
+    connection.release();
   });
 }
