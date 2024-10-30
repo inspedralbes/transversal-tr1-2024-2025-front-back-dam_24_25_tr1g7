d73c8ae67f0e26b5d76282591d41cce20bd368ca
a23poldiabel <a23poldiabel@inspedralbes.cat>
2024-10-30 09:35:12 +0100

    Falta mirar si hay mas de una semana y mes

--- vue/src/pages/veureEstadistiques.vue
+++ vue/src/pages/veureEstadistiques.vue
@@ -4,8 +4,17 @@
 
         <v-main>
             <v-container class="mt-10">
-                <!-- Selector de fechas -->
+                <!-- Selector de tipo de informe -->
                 <v-select
+                    v-model="tipoSeleccionado"
+                    :items="['fechas', 'semanales', 'mensuales']"
+                    label="Selecciona el tipo de informe"
+                    @change="cargarInformes"
+                />
+
+                <!-- Selector de fechas si el tipo es 'fechas' -->
+                <v-select
+                    v-if="tipoSeleccionado === 'fechas'"
                     v-model="fechaSeleccionada"
                     :items="fechas"
                     label="Selecciona una fecha"
@@ -17,13 +26,13 @@
                     <v-col cols="12" md="6" v-for="imagen in imagenes" :key="imagen">
                         <v-card>
                             <v-img
-                                :src="`http://dam.inspedralbes.cat:21345/informes/${fechaSeleccionada}/${imagen}`"
+                                :src="imagen"
                                 height="350px"
                             ></v-img>
                         </v-card>
                     </v-col>
                 </v-row>
-                <v-alert v-else type="info">Selecciona una fecha para ver los informes.</v-alert>
+                <v-alert v-else type="info">Selecciona un tipo de informe y, si es necesario, una fecha.</v-alert>
             </v-container>
         </v-main>
     </v-app>
@@ -34,6 +43,7 @@ export default {
     name: 'Estadistiques',
     data() {
         return {
+            tipoSeleccionado: null,
             fechaSeleccionada: null,
             fechas: [],
             imagenes: [],
@@ -41,26 +51,55 @@ export default {
         };
     },
     created() {
-        this.cargarFechas();
+        this.cargarInformes();
     },
     methods: {
-        async cargarFechas() {
-        try {
-            const response = await fetch('http://dam.inspedralbes.cat:21345/listarInformes');
-            if (!response.ok) throw new Error("Error al cargar las fechas");
-            this.informes = await response.json();
-            this.fechas = Object.keys(this.informes); // Obtener las fechas desde los nombres de las carpetas
-        } catch (error) {
-            console.error("Error cargando las fechas:", error);
+        async cargarInformes() {
+            try {
+                const response = await fetch('http://localhost:21345/listarInformes');
+                if (!response.ok) throw new Error("Error al cargar los informes");
+                this.informes = await response.json();
+
+                // Extraer fechas como opciones si el tipo de informe es "fechas"
+                if (this.tipoSeleccionado === 'fechas') {
+                    this.fechas = Object.keys(this.informes.fechas);
+                }
+
+                // Cargar imágenes según el tipo seleccionado
+                this.actualizarImagenes();
+            } catch (error) {
+                console.error("Error cargando los informes:", error);
+            }
+        },
+        actualizarImagenes() {
+            if (this.tipoSeleccionado === 'fechas' && this.fechaSeleccionada) {
+                this.imagenes = (this.informes.fechas[this.fechaSeleccionada] || []).map(
+                    img => `http://localhost:21345/informes/${this.fechaSeleccionada}/${img}`
+                );
+            } else if (this.tipoSeleccionado === 'semanales') {
+                this.imagenes = (this.informes.semanales || []).map(
+                    img => `http://localhost:21345/informes/semanales/${img}`
+                );
+            } else if (this.tipoSeleccionado === 'mensuales') {
+                this.imagenes = (this.informes.mensuales || []).map(
+                    img => `http://localhost:21345/informes/mensuales/${img}`
+                );
+            } else {
+                this.imagenes = [];
+            }
+
+            // Imprimir las rutas de las imágenes generadas para depuración
+            console.log("Imágenes a mostrar:", this.imagenes);
         }
     },
-    cargarInformes() {
-        if (!this.fechaSeleccionada) return;
-
-        // Cargar las imágenes según la fecha seleccionada
-        this.imagenes = this.informes[this.fechaSeleccionada] || [];
-        this.imagenes = this.imagenes.map(imagen => `http://dam.inspedralbes.cat:21345/informes/${this.fechaSeleccionada}/${imagen}`);
-    }
+    watch: {
+        tipoSeleccionado() {
+            this.fechaSeleccionada = null; // Reiniciar la fecha cuando se cambia el tipo de informe
+            this.cargarInformes(); // Volver a cargar los informes
+        },
+        fechaSeleccionada() {
+            this.actualizarImagenes(); // Actualizar las imágenes al cambiar la fecha
+        }
     }
 };
 </script>
